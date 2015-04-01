936586421ffc60bf7ca7708edb4a2554c8c4e9ae
asinusware <danielzone46@hotmail.com>
2015-04-01 01:22:17 -0600

    Base
    Se crea un reloj insipiente

---
+++ Practica02/Practica02/Program.cs
@@ -0,0 +1,32 @@
+﻿/* * * * * * * * * * *
+ * PRACTICA O2-1 RELOJ
+ * DANIEL CEDILLO GUEVARA
+ * ING. EN CIENCIAS COMPUTACIONALES
+ * CUTONALA
+ * PROGRAMACION III
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace reloj_cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (; ; ) //esto genera un ciclo infinito
+            {
+                DateTime fecha = DateTime.Now;
+                Console.Clear();
+                Console.WriteLine(fecha.TimeOfDay); //imprime la fecha y hora actual del sistema
+                Thread.Sleep(1000); //pausa de 1 segundo
+            }
+
+        }
+    }
+}
